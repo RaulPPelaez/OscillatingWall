a4b2160d65293fcde4ae5e4de47c0132b254dcfb
Raul P. Pelaez <raul.perez@uam.es>
2018-04-27 16:41:32 +0200

    Add Elastic_Network_Model code

--- genWall.bash
+++ genWall.bash
@@ -13,8 +13,8 @@ radius=$(echo 1 | awk '{print 2^(1/6.)}')
 g++ -std=c++11 -O3 genWall.cpp -o genWall
 
 ./genWall $Lx $Ly $nlayers $radius > wall.init
-
-ENM wall.init 1.2 200 $Lx $Ly 10000 > wall.bonds
+g++ -std=c++11 -O3 Elastic_Network_Model.cpp -o ENM
+./ENM wall.init 1.2 200 $Lx $Ly 10000 > wall.bonds
 N=$(head -1 wall.init)
 
 echo 0 > wall.bondsFP
@@ -23,4 +23,4 @@ echo $N >> wall.bondsFP
 tail -n+2 wall.init | awk '{print NR-1, $1,$2,$3, 200, 0}' >> wall.bondsFP
 
 
-rm genWall
+rm genWall ENM
